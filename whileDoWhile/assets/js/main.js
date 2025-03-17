ea1566cfcbf4f7c9b6598323d82a0e849b68854e
Matheus Martins <matheuss_fernandes@outlook.com>
2025-03-17 17:53:56 -0300

    while, do while

---
+++ whileDoWhile/assets/js/main.js
@@ -0,0 +1,20 @@
+function random(min, max) {
+    const r = Math.random() * (max - min) + min;
+    return Math.floor(r);
+}
+
+let min = 1, max = 50;
+
+let rand = 10;
+
+
+while (rand !== 10) {
+    rand = random(min, max);
+    console.log(rand);
+}
+console.log('#####');
+
+do {
+    rand = random(min, max);
+    console.log(rand);
+} while (rand !== 10);
