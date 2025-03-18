00fa12c72dc705f277ea405e696603137ee9fa41
Matheus Martins <matheuss_fernandes@outlook.com>
2025-03-18 13:40:24 -0300

    ajuste

--- exerciciosLogica60/assets/js/fizzBuzz.js
+++ exerciciosLogica60/assets/js/fizzBuzz.js
@@ -1,6 +1,6 @@
 // Exercício com lógica de programação - 03
 function fizzBuzz(numero) {
-    if (!(typeof numero === 'number')) numero;
+    if (typeof numero !== 'number') return numero;
     if (numero % 3 === 0 && numero % 5 === 0) return 'FizzBuzz';
     if (numero % 5 === 0) return 'Buzz';
     if (numero % 3 === 0) return 'Fizz';
