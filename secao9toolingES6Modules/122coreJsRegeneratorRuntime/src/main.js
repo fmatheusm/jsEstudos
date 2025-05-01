8cab515cf9bc692fb3bd3180d2e0e1f746512492
Matheus Martins <matheuss_fernandes@outlook.com>
2025-05-01 01:47:33 -0300

    garantindo que funcione em navegadores mais antigos

---
+++ secao9toolingES6Modules/122coreJsRegeneratorRuntime/src/main.js
@@ -0,0 +1,5 @@
+import 'core-js/stable'; // garante que funciona em navegadores mais antigos
+import 'regenerator-runtime/runtime'; // garante que funciona em navegadores mais antigos
+import executa from './modules/promises';
+import './assets/css/style.css';
+executa();
